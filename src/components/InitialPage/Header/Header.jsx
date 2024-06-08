41068fd2f69f947748a9928579220386f38e579b
Jack Huang <jfuhuang@iastate.edu>
2024-06-08 12:28:48 -0500

    Fixed initial page error with PlayingCard props.

--- src/components/InitialPage/Header/Header.jsx
+++ src/components/InitialPage/Header/Header.jsx
@@ -3,12 +3,16 @@ import './Header.css';
 import PlayingCard from 'components/PlayingCard.jsx';
 
 function Header() {
+
+    const initialCard1 = {id: 1, value: 1, name: "ace"};
+    const initialCard2 = {id: 2, value: 1, name: "ace"};
+
     return (
         <header className="p-0">
             <h1 className="m-5 p-0 display-1 text-center">Krypto<br /> The Card Game</h1>
             <div style = {{gap: "100px"}} className="cards d-flex align-items-center justify-content-center flex-nowrap m-0 p-0 w-100">
-                <PlayingCard card={{name: "ace"}}/>
-                <PlayingCard card={{name: "ace"}}/>
+                <PlayingCard cardData={initialCard1} cardDimensions = {{width: "12.1875rem", height: "17.75rem"}} isInteractive = {true}/>
+                <PlayingCard cardData={initialCard2} cardDimensions = {{width: "12.1875rem", height: "17.75rem"}} isInteractive = {true}/>
             </div>
         </header>
     );
