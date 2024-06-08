a1bd82a667d7dccb4d996b870be35d09f89050fd
Jack Huang <jackfuhuang@gmail.com>
2024-06-08 14:09:59 -0500

    Fixed Header and PlayingCard

--- src/components/InitialPage/Header/Header.jsx
+++ src/components/InitialPage/Header/Header.jsx
@@ -3,12 +3,17 @@ import './Header.css';
 import PlayingCard from 'components/PlayingCard.jsx';
 
 function Header() {
+    const cardDimensions = {
+        width: "8.4375rem",
+        height: "12.4375rem",
+    };
+
     return (
         <header className="p-0">
             <h1 className="m-5 p-0 display-1 text-center">Krypto<br /> The Card Game</h1>
             <div style = {{gap: "100px"}} className="cards d-flex align-items-center justify-content-center flex-nowrap m-0 p-0 w-100">
-                <PlayingCard card={{name: "ace"}}/>
-                <PlayingCard card={{name: "ace"}}/>
+                <PlayingCard cardData={{name: "ace"}} cardDimensions={cardDimensions} isInteractive={true}/>
+                <PlayingCard cardData={{name: "ace"}} cardDimensions={cardDimensions} isInteractive={true}/>
             </div>
         </header>
     );
--- src/components/PlayingCard.jsx
+++ src/components/PlayingCard.jsx
@@ -13,23 +13,13 @@ export default function PlayingCard({cardData, cardDimensions, isInteractive}) {
         maxHeight: 300,
         padding: 0,
         cursor: isInteractive ? 'pointer' : 'default',
-        backgroundImage: isInteractive ? `url(${imagePath})` : 'none',
-        backgroundPosition: '50%',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor: 'lightgray',
     }
 
-    const handleDragStart = (event) => {
-        event.dataTransfer.setData('text/plain', cardData.id);
-    }
 
     return (
         <div 
             className={`cardContainer container d-flex justify-content-center align-items-center ${isInteractive ? 'interactive' : ''}`} 
             style={{...cardContainerStyle}}
-            draggable={isInteractive}
-            onDragStart={isInteractive ? handleDragStart : null}
         >
             <img className="img-fluid" src={imagePath} alt={cardData.name} />
         </div>
